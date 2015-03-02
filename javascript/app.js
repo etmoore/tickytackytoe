1a14e90362bbbaac1c73b9a731ed548481cf857a
Evan Moore <etmoore@gmail.com>
2015-03-02 15:34:45 -0700

    users can drop marks in empty cells, players switch

--- javascript/app.js
+++ javascript/app.js
@@ -1,1 +1,31 @@
+$(function () {
+  'use strict';
 
+  var App = {
+    init: function() {
+      this.setEventListeners();
+      this.playerOne = true;
+    },
+    setEventListeners: function() {
+      var self = this;
+      $('.board').on('click', '.cell', function(){
+        if ( self.cellEmpty(this) ){
+          self.dropMark(this);
+          self.switchPlayer();
+        }
+      });
+    },
+    cellEmpty: function(cell) {
+      return $(cell).is(':empty');
+    },
+    dropMark: function(cell) {
+      if (this.playerOne){ $(cell).html('O'); }
+      else { $(cell).html('X'); }
+    },
+    switchPlayer: function() {
+      this.playerOne = !this.playerOne;
+    }
+  };
+
+  App.init();
+}());
